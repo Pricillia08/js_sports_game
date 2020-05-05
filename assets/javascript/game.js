73a90f33bc7618324d3bc9ae7f82c6b57efcdd0c
Pricillia Polynice <ppolynice817@gmail.com>
2020-05-04 23:21:49 -0500

    Initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+let reset = 0
+const numResets = document.querySelector("#num-resets");
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", Math.random ()){
+    console.log("+button clicked"); 
+}
+
+let shotsTaken = document.querySelector("#teamone-numshots");
+let numGoals = document.querySelector("#teamone-numgoals");
+let goals = document.querySelector("#goals")
+shotsTaken.addEventListener("click",Math.random(){
+console.log("+button clicked");
+})
+shotsTaken.addEventListener("click", Math.random(){
+    console.log("+button clicked")
+
+    let shotsTaken = Number(counter.innerHTML) + 1;}
+
+
+let shotsTaken = document.querySelector("#teamtwo-numshots");
+let numGoals = document.querySelector("#teamtwo-numgoals");
+let goals = document.querySelector("#goals")
+shotsTaken.addEventListener("click",Math.random(1){
+console.log("+button clicked");
+})
+shotsTaken.addEventListener("click", function(){
+    console.log("+button clicked");
+    
+    let shotsTaken = Number(counter.innerHTML) + 1){
